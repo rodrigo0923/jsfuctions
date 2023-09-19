d6d5eab143dad50d33afceff4afb1656dce55ea2
rodrigo0923 <rodrigo.yanez1401@gmail.com>
2023-09-19 17:03:43 -0300

    jsfuctions

---
+++ fuctions.js
@@ -0,0 +1,10 @@
+function custom() {
+    // podemos incluir más código aquí si queremos
+    console.log("this page says ninja was liked");
+}
+function change(element) {
+    element.innerText = "log out";
+}
+function hide(element) {
+    element.remove();
+}
